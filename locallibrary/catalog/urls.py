254abd43277c22f3746c0de86495fb9d1f27c818
victorjunh4-dev <victorjunh4@gmail.com>
2025-11-13 07:55:41 -0300

    Atualização: Página de visualização do usuário

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from catalog import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('authors/', views.AuthorsListView.as_view(), name='authors')
 ]
